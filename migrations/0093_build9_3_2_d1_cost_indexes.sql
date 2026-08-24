c9c3740f0fdf86c7efd25adc6e8b0614831ef1de
Christopher Kalupa <kalupa@gmail.com>
2026-08-24 12:37:08 -0500

    K-Prop Build 9.3.2 D1 play-slip query cost index

---
+++ migrations/0093_build9_3_2_d1_cost_indexes.sql
@@ -0,0 +1,4 @@
+﻿-- K-Prop Build 9.3.2
+-- D1 cost optimization for play-slip settlement.
+CREATE INDEX IF NOT EXISTS idx_feature_snapshots_prop_model_latest
+ON feature_snapshots(prop_id,model_version_id,snapshot_time DESC,feature_snapshot_id DESC);
